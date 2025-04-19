9c61d7fde33bcc2c4987290d50a6932603b4f684
Paniznik Aryna <arinapaniznik4@gmail.com>
2025-04-19 23:29:52 +0300

    add Course Entity

--- src/main/java/com/houseoflyrics/backend/entity/Course.java
+++ src/main/java/com/houseoflyrics/backend/entity/Course.java
@@ -1,5 +1,60 @@
 package com.houseoflyrics.backend.entity;
 
-public class Course {
+import jakarta.persistence.*;
 
+@Entity
+@Table(name = "course")
+public class Course {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "title", length = 255, nullable = false)
+    private String title;
+
+    @Column(name = "picture", length = 255, nullable = false)
+    private String picture;
+
+    @Column(name = "description", nullable = false, columnDefinition = "TEXT")
+    private String description;
+
+    public Course(){
+    }
+    public Course(String title, String picture, String description) {
+        this.title = title;
+        this.picture = picture;
+        this.description = description;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getPicture() {
+        return picture;
+    }
+
+    public void setPicture(String picture) {
+        this.picture = picture;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
 }
